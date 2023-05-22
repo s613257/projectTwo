34dcc2d8d9fabcf519992b57eb8aa1b0138bc58f
s613257 <s613257@gmail.com>
2023-05-22 10:47:31 +0800

    0522

--- projectTwo/src/main/java/hibernate/Action.java
+++ projectTwo/src/main/java/hibernate/Action.java
@@ -22,20 +22,20 @@ public class Action {
 			session.beginTransaction();
 			
 			BookingTkDAO hDao = new BookingTkDAOImpl(session);
-			//新增單筆
-			BookingTk bookingTk = new BookingTk();
-			bookingTk.setTicketID(200);
-			bookingTk.setTranNo("102");
-			bookingTk.setSeat("01A");
-			bookingTk.setDepartureST("1");
-			bookingTk.setDestinationST("2");
-			bookingTk.setdeparturedate("2023-05-19");
-			bookingTk.setDeparturetime("06:00");
-			bookingTk.setArrivaltime("06:15");
-			bookingTk.setPrice(100);
-			bookingTk.setbookingdate("2023-05-19");
-			hDao.insert(bookingTk);
-			
+//			//新增單筆
+//			BookingTk bookingTk = new BookingTk();
+//			bookingTk.setTicketID(200);
+//			bookingTk.setTranNo("102");
+//			bookingTk.setSeat("01A");
+//			bookingTk.setDepartureST("1");
+//			bookingTk.setDestinationST("2");
+//			bookingTk.setdeparturedate("2023-05-19");
+//			bookingTk.setDeparturetime("06:00");
+//			bookingTk.setArrivaltime("06:15");
+//			bookingTk.setPrice(100);
+//			bookingTk.setbookingdate("2023-05-19");
+//			hDao.insert(bookingTk);
+//			
 			
 			//查詢全部
 //			List<BookingTk> lists = hDao.selectAll();
@@ -46,41 +46,41 @@ public class Action {
 //			}
 			
 			//selectById
-			BookingTkService service = new BookingTkServiceImpl(session);
-			TicketDTO result = (new TicketDAOImpl()).GetTicketInfoById("200");
-			if(result != null) {
-				System.out.println("oridata===============================");
-				System.out.println(result.getTicketID() + " " + result.getTranNo()+" "+ result.getSeat()+" "+
-			result.getDepartureST()+" "+result.getDestinationST()+" "+result.getDeparturedate()+" "+
-			result.getDeparturetime()+" "+result.getArrivaltime()+" "+result.getPrice()+" "+result.getBookingdate());
-			}
-			
-			result.setTranNo("200");
-			
-			bookingTk.setTicketID(result.getTicketID());
-			bookingTk.setTranNo(result.getTranNo());
-			bookingTk.setSeat(result.getSeat());
-			bookingTk.setDepartureST(result.getDepartureST());
-			bookingTk.setDestinationST(result.getDestinationST());
-			bookingTk.setdeparturedate(result.getDeparturedate());
-			bookingTk.setDeparturetime(result.getDeparturetime());
-			bookingTk.setArrivaltime(result.getArrivaltime());
-			bookingTk.setPrice(result.getPrice());
-			bookingTk.setbookingdate(result.getBookingdate());
-			service.update(bookingTk);
-			
-			bookingTk = service.selectById(200);
-			if(result != null) {
-				System.out.println("updatadata===============================");
-				System.out.println(result.getTicketID() + " " + result.getTranNo()+" "+ result.getSeat()+" "+
-			result.getDepartureST()+" "+result.getDestinationST()+" "+result.getDeparturedate()+" "+
-			result.getDeparturetime()+" "+result.getArrivaltime()+" "+result.getPrice()+" "+result.getBookingdate());
-			}
+//			BookingTkService service = new BookingTkServiceImpl(session);
+//			TicketDTO result = (new TicketDAOImpl()).GetTicketInfoById("200");
+//			if(result != null) {
+//				System.out.println("oridata===============================");
+//				System.out.println(result.getTicketID() + " " + result.getTranNo()+" "+ result.getSeat()+" "+
+//			result.getDepartureST()+" "+result.getDestinationST()+" "+result.getDeparturedate()+" "+
+//			result.getDeparturetime()+" "+result.getArrivaltime()+" "+result.getPrice()+" "+result.getBookingdate());
+//			}
+//			
+//			result.setTranNo("200");
+//			
+//			bookingTk.setTicketID(result.getTicketID());
+//			bookingTk.setTranNo(result.getTranNo());
+//			bookingTk.setSeat(result.getSeat());
+//			bookingTk.setDepartureST(result.getDepartureST());
+//			bookingTk.setDestinationST(result.getDestinationST());
+//			bookingTk.setdeparturedate(result.getDeparturedate());
+//			bookingTk.setDeparturetime(result.getDeparturetime());
+//			bookingTk.setArrivaltime(result.getArrivaltime());
+//			bookingTk.setPrice(result.getPrice());
+//			bookingTk.setbookingdate(result.getBookingdate());
+//			service.update(bookingTk);
+//			
+//			bookingTk = service.selectById(200);
+//			if(result != null) {
+//				System.out.println("updatadata===============================");
+//				System.out.println(result.getTicketID() + " " + result.getTranNo()+" "+ result.getSeat()+" "+
+//			result.getDepartureST()+" "+result.getDestinationST()+" "+result.getDeparturedate()+" "+
+//			result.getDeparturetime()+" "+result.getArrivaltime()+" "+result.getPrice()+" "+result.getBookingdate());
+//			}
 		
 			
 			//delete
-//			int status = service.delete(200);
-//			System.out.println("status:" + status);
+			int status = service.delete(200);
+			System.out.println("status:" + status);
 			
 			session.getTransaction().commit();
 		} catch (Exception e) {
